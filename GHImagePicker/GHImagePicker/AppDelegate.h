ecd40aa1877f37cd86f20c86338d2bfb08157ee0
Rong <ronginet@RongdeMacBook-Pro.local>
2018-02-23 23:51:53 +0800

    添加项目

---
+++ GHImagePicker/GHImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHImagePicker
+//
+//  Created by Rong on 2018/2/23.
+//  Copyright © 2018年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHImagePicker/GHImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHImagePicker
+//
+//  Created by Rong on 2018/2/23.
+//  Copyright © 2018年 Rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
