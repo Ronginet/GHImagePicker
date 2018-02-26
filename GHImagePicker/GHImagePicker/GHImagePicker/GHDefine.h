170cabeecaf566ac7e0d2a2f2667874037d07a91
Rong <ronginet@RongdeMacBook-Pro.local>
2018-02-26 23:35:09 +0800

    添加GHDefine文件

---
+++ GHImagePicker/GHImagePicker/GHImagePicker/GHDefine.h
@@ -0,0 +1,15 @@
+
+//
+//  GHDefine.h
+//  GHImagePicker
+//
+//  Created by Rong on 2018/2/26.
+//  Copyright © 2018年 Rong. All rights reserved.
+//
+
+#ifndef GHDefine_h
+#define GHDefine_h
+
+#define GHRGBColor(r,g,b)  [UIColor colorWithRed:(r) / 255.0 green:(g) / 255.0 blue:(b) / 255.0 alpha:1.0]
+
+#endif /* GHDefine_h */
